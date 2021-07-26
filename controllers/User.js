b4c8f2710a739b6b065cd24f12d24e9a6bf87156
unknown <geonfak76@gmail.com>
2021-07-26 12:59:31 +0300

    finished the db controllers for users,might need some refactor and more proper sql queries

--- controllers/User.js
+++ controllers/User.js
@@ -7,56 +7,78 @@ const jwtSecret = "my house is on fire";
 const jwt = require("jsonwebtoken");
 
 module.exports.getUserInfo = async function (req, res) {
-  //we need his posts ,his comments along with post ids and we need his friends(requeststatus="accepted")
-  //also his groups
-  //gonna separate it
-  try {
-  } catch (error) {}
+  const user_id = await findUserIdByName(req.query.name);
   let userGroups, userFriends, userPosts, userComments;
+};
+
+module.exports.getUserGroups = async function (req, res) {
+  const sqlStatement = "SELECT group_id from isMemberOfGroup WHERE user_id=$1";
 
   try {
     userGroups = await poolDB.query(
-      "SELECT group_id from isMemberOfGroup WHERE user_id=$1",
+      sqlStatement,
       [requestedUserId]
     );
   } catch (error) {
     console.error(error);
   }
-
+};
+module.exports.getUserComments = async function (req, res) {
+  let userCommentsDbReturn;
+  const sqlStatement;
   try {
+    userCommentsDbReturn = await poolDB.query("");
   } catch (error) {
     console.error(error);
+    res.status(400).send("Couldnt get user comments");
   }
 };
-
 module.exports.getUserFriends = function (req, res) {
   // SELECT name FROM users where user_id = (
   // SELECT user2 FROM areFriends where user1={req.user._id});
-  console.log("hello");
+  const sqlStatement = "SELECT name FROM users where user_id = (SELECT name from areFriends where user1=$1 and friendrequest_status='accepted')"
+
+  let userFriendsDbReturn;
+try {
+     userFriendsDbReturn = await poolDB.query(sqlStatement,[req.query.user])
+      res.status(200).send(userFriendsDbReturn.rows)
+} catch (err) {
+  
+}  console.error(err)
 };
 
 module.exports.getUserPosts = function (req, res) {
-  console.log("hello");
-  //SELECT * FROM posts WHERE author_id = {req.user._id};
+  const sqlStatement = "SELECT * FROM posts WHERE author_id=$1";
+
+  try {
+    const userPostsDbReturn = await poolDB.query(sqlStatement,[req.params.id])
+
+    res.status(200).json(userPostsDbReturn.rows)
+
+  } catch (error) {
+    console.error(error);
+    res.status(400).send("The request for user posts failed")
+  }
 };
 
 module.exports.registerUser = async function (req, res) {
   //todo if email already exists reject it
   //this saves the user properly in the database
+  const existingUser = await poolDB.query("SELECT * FROM users where email=$1",[req.body.email])
+  if(existingUser)return res.status(406).send(`User with email: ${req.body.email} already exists`)
   const salt = await bcrypt.genSalt(10);
-  // //?
   const hashedPassword = await bcrypt.hash(req.body.password, salt);
   const user = {
     name: req.body.name,
     email: req.body.email,
     password: hashedPassword,
   };
-
+  const sqlStatement = "INSERT INTO users (name,email,password) VALUES ($1,$2,$3);" 
   //todo save user to database
   console.log(user);
   try {
     const saved = await poolDB.query(
-      "INSERT INTO users (name,email,password) VALUES ($1,$2,$3);",
+     sqlStatement,
       [req.body.name, req.body.email, hashedPassword]
     );
     console.log("success");
@@ -68,7 +90,9 @@ module.exports.registerUser = async function (req, res) {
 };
 
 module.exports.loginUser = async function (req, res) {
-  const user = await poolDB.query("SELECT * FROM users WHERE email=$1", [
+  const sqlStatement = "SELECT * FROM users WHERE email=$1";
+
+  const user = await poolDB.query(sqlStatement, [
     req.body.email,
   ]);
   if (!user) return res.status(401).send("Email or password is wrong");
@@ -95,3 +119,14 @@ module.exports.logoutUser = async function (req, res) {
   res.set("auth-token", "");
   res.status(200).send("succesfully logged out");
 };
+
+async function findUserIdByName(name) {
+  const sqlStatement = "SELECT user_id from users WHERE name=$1"
+
+  const userDbReturn = await poolDB.query(
+    sqlStatement,
+    [name]
+  );
+  const user_id = userDbReturn.rows[0].user_id;
+  return user_id;
+}
